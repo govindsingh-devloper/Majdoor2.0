074a0405fed3a59ecd3389b7abb03dd22c19825a
govindsingh-devloper <152960266+govindsingh-devloper@users.noreply.github.com>
2024-06-05 13:34:15 +0530

    Custome Page

--- frontend/src/components/core/auth/Customer/CustHome.js
+++ frontend/src/components/core/auth/Customer/CustHome.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom';
 
 const CustHome = () => {
+  const { user } = useSelector((state) => state.profile)
+  const navigate=useNavigate();
+
   return (
-    <div>CustHome</div>
+    <div>
+      <h1>My Profile</h1>
+      <img
+        src={user?.image}
+      />
+      <p>{user?.firstName + " "+user?.lastName}</p>
+      <p>{user?.email}</p>
+      <p>{user?.additionalDetails?.about ?? "Write Something About Yourself"}</p>
+    </div>
   )
 }
 
