75e3b9411de6b48998c19b5fceb0438c20266613
mansbisht-12 <85402717+mansibisht-12@users.noreply.github.com>
2024-06-14 15:44:27 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,9 +6,15 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Homes from "./pages/Home";
 import Header from './components/common/Header';
 import Footer from './components/common/Footer';
+
+
+// import Services from "./components/common/Services";
+// import Gallery from './components/common/Gallery';
+// import Facilities from './components/common/Facilities';
+// import Reviews from './components/common/Reviews';
+
 import Login from "./pages/login";
 import Signup from "./pages/signup";
-
 import CustomerDashboard from './components/core/auth/Customer/CustomerDashboard';
 import MajdoorDashboard from './components/core/auth/Majdoor/MajdoorDashboard';
 import VerifyEmail from "./pages/VerifyEmail"
@@ -30,11 +36,9 @@ import TSignupform from "./components/core/auth/TSignupform";
 import Edit from "./components/core/auth/Customer/Edit";
 import ChangeProfilePicture from "./components/core/auth/Customer/Edit/ChangeProfilePicture";
 import MajdoorConfirmation from "./components/core/auth/Majdoor/MajdoorConfirmation";
-import ThekedarDashbord from '../src/components/core/auth/thekedar/ThekedarDashbord'
-import CustomerSidebar from '../src/components/core/auth/Customer/CustomerSidebar'
-import CustomerLayout from './components/core/auth/Customer/CustomerLayout';
 import ThekedarDashbord from '../src/components/core/auth/thekedar/ThekedarDashbord';
 import SelectedMajdoor from "./components/core/auth/Customer/selectedMajdoor";
+import CustomerLayout from "./components/core/auth/Customer/CustomerLayout";
 
 
 
@@ -70,23 +74,21 @@ const App = () => {
 
           <Routes>
             <Route path="/" element={<Homes/>}/>
+            
             <Route path="/verify-email" element={<VerifyEmail/>}/>
             <Route path='/login' element={<Login/>}/>
             <Route path='/signup' element={<Signupform/>}/>
             <Route path='/forgotpassword' element={<ForgotPassword/>}/>
             <Route path='/Update-password/:id' element={<UpdatePassword/>}/>
-           
-           {/* loginsignup */}
+                        
+            {/* login/signup */}
             <Route path='/majdoor-login' element={<LoginForm/>}/>
             <Route path='/majdoor-signup' element={<Signupform/>}/>
             <Route path="/customer-login" element={<Loginform/>}/>
             <Route path="/thekedar-login" element={<Loginform/>}/>
             <Route path="/thekedar-signup" element={<TSignupform/>}/>
-            
-            {/* components */}
             <Route path="/contact" element={<Contact />} />
-            <Route path="/Mdashboard" element={<Mdashboard />} />
-            <Route path="/searchMajdoor" element={<SearchMajdoor/>}/>
+            
             <Route path="/about" element={<About />} />
             <Route path="/reviews" element={<Reviews />} />
             <Route path="/services" element={<Services />} />
@@ -101,21 +103,21 @@ const App = () => {
                 <Route path="/CustomerHome" element={<CustomerHome />} />
                 <Route path="/CustomerDashboard/Edit" element={<Edit />} />
                 
-
             </Route>
-
-            <Route path="/customersidebar" element={<CustomerSidebar />} />
-
-            {/* Thekedaar */}
-            <Route path="/ThekedarDashbord" element={<ThekedarDashbord />} />
+            <Route path="/CustomerDashboard/Edit" element={<Edit />}/>
+            <Route path="/searchMajdoor/:id" element={<SelectedMajdoor/>}/>
             
-            {/* Majdoor */}
+            {/* majdoor */}
+            <Route path="/Mdashboard" element={<Mdashboard />} />
             <Route path="/MajdoorDashboard" element={<MajdoorDashboard />} />
             <Route path="/MajdoorConfirmation" element={<MajdoorConfirmation />} />
-            <Route path="/Mdashboard" element={<Mdashboard />} />
+              
+              
+            {/* thekedar */}
+            <Route path="/ThekedarDashbord" element={<ThekedarDashbord />} />
 
             </Routes>
-            
+          
           <Footer /> 
           </div>
           
