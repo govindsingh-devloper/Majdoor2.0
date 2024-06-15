c39aefd5c70adba8078a07fcf4f60dff6ca93218
Abhishekkardam <abhishekkardamm@gmail.com>
2024-06-15 17:19:19 +0530

    Work Done

--- frontend/src/components/core/auth/thekedar/ThekedarDashboard.jsx
+++ frontend/src/components/core/auth/thekedar/ThekedarDashboard.jsx
@@ -3,7 +3,7 @@
  import Sidebar from './ThekedarSidebar';
  import MainContent from './MainContent';
 
-const ThekedarDashbord = () => {
+const ThekedarDashboard = () => {
   const [activeContent, setActiveContent] = useState('डैशबोर्ड');
 
   const handleContentChange = (content) => {
@@ -20,18 +20,4 @@ const ThekedarDashbord = () => {
   );
 };
 
-export default ThekedarDashbord;
-
-
-// export default function Thekedar(){
-//   return(
-//     <>
-//       <div className="dashboard-container flex">
-// //       {/* Sidebar component */}
-// //       <Sidebar activeContent={activeContent} onContentChange={handleContentChange} />
-// //       {/* MainContent component positioned to the right of the Sidebar */}
-// //       <MainContent activeContent={activeContent} />
-// //     </div>
-//     </>
-//   )
-// }
+export default ThekedarDashboard;
